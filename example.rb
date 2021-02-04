2715dfb3acf943b313e13c12c43125b589a2e3fc
Sean Brown <applebaumxyz@gmail.com>
2021-02-04 01:10:08 +0000

    Automatically backed up by Learn

---
+++ example.rb
@@ -0,0 +1,12 @@
+answer = "flamboyance"
+puts "A group of flamingos is called a #{answer}."
+
+answer
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+stylish_chef
